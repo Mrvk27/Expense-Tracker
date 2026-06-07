7f11205c447698cfaaf5a772f562e253ecd29a84
Mrvk27 <vaibhavk27k@gmail.com>
2026-06-07 12:26:50 +0530

    lecture 7

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,make_response,flash,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
+from datetime import date,datetime
 
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+app.config['SECRET_KEY']='my-secret-key'
 db=SQLAlchemy(app)
 
 
@@ -23,6 +24,42 @@ with app.app_context():
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route("/add",methods=['POST'])
+def add():
+    description=(request.form.get("description") or "").strip()
+    amount_str=(request.form.get("amount") or "").strip()
+    category=(request.form.get("category") or "").strip()
+    date_str=(request.form.get("date") or "").strip()
+
+
+    if not description or not amount_str or not category:
+        flash("Please Fill Description,amount,category","error")
+        return redirect(url_for("index"))
+
+    try:
+        amount=float(amount_str)
+        if amount <=0:
+            raise ValueError
+    except ValueError:
+        flash("Amount must be postive","error")
+        return redirect(url_for("index"))
+    
+    try:
+        d=datetime.strptime(date_str,"%Y-%m-%d").date() if date_str else date.today()
+    except ValueError:
+        d=date.today
+
+    e=Expense(description=description,amount=amount,category=category,date=d)
+    db.session.add(e)
+    db.session.commit()
+
+    flash("Expense Added","success")
+    return redirect(url_for("index"))
+
+    print("Form Received",dict(request.form))
+    return make_response("Form Received check the console")
+
 
 if __name__=='__main__':
     app.run(debug=True)
